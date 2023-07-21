c7b5f0b5f209a1319d51c93d74f3dc67e9e1098a
cem.ugurlu <cem.ugurlu@cemugurlu.ekinoks.io>
2023-07-21 10:22:51 +0300

    Proje Oluşturma

---
+++ 3. Proje Oluşturma/Java Application/src/Deneme.java
@@ -0,0 +1,8 @@
+public class Deneme {
+    public static void main(String[] args){
+        int a;
+        a = 3;
+        System.out.println("Hello world");
+        System.out.println(a);
+    }
+}
