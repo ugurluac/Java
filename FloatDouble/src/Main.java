9a823987a1e7051a189f5527d628104a78cdde76
cem.ugurlu <cem.ugurlu@cemugurlu.ekinoks.io>
2023-07-21 10:41:01 +0300

    FloatDouble

---
+++ FloatDouble/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+   
+
+        
+        double sayi1 = 70.25;
+        double sayi2 = 60d;
+        double sayi3 = 80.2;
+        
+        
+        float a = 70.25f;
+        float b=60f;
+        float c=80.2f;
+        System.out.println( (sayi1 + sayi2 + sayi3)/3);
+    
+    }
+}
