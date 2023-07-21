458b62321fd91b973ec2fed89e54a8557cfb4792
cem.ugurlu <cem.ugurlu@cemugurlu.ekinoks.io>
2023-07-21 10:38:50 +0300

    Faiz

---
+++ FaizUygulaması/src/Main.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Faiz Uygulaması
+        Kullanıcıdan anapara değerini ve parasını kaç yıl vadeli yatırmak istediği
+        bilgisini alın ve her sene sonunda toplam para miktarını ekrana yazdırın.
+        
+        Faiz Oranı : %6 Faiz Oranı
+
+        */
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Bankamiza Hos Geldiniz... \nFaiz Oran %6");
+        
+        int anapara,vade;
+        
+        System.out.println("Yatirmak istediginiz Tutar :");
+        anapara = scanner.nextInt();
+        System.out.println("Paranizi kac yil vadeli yatirmak istersiniz?");
+        vade = scanner.nextInt();
+        
+        double toplampara = anapara;
+        double faizOrani = 0.06;
+        
+        for (int i = 1 ; i <= vade ; i++) {
+        
+        	toplampara = (toplampara * faizOrani) + toplampara;
+        
+        	System.out.println(i + ". yilin sonunda toplam para : " + toplampara);
+        
+    }
+}
+}
