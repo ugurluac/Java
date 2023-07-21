de4b36cbf745d48c2dc6b478c7bfd924b8697e8f
cem.ugurlu <cem.ugurlu@cemugurlu.ekinoks.io>
2023-07-21 11:00:28 +0300

    Referanslar

---
+++ Referanslar/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Account account1 = new Account("Cem U", "example@gmail.com", "34234");
+
+		account1.bilgilerigoster();
+	}
+
+}
