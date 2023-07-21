958b5ab380d0a1bf9c7ae2b3ebdd70f8bd5d847d
cem.ugurlu <cem.ugurlu@cemugurlu.ekinoks.io>
2023-07-21 10:58:36 +0300

    OOPGiris

---
+++ OOPGiris/src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+	public static void main(String[] args) {
+
+		Araba araba1 = new Araba();
+		araba1.renk = "Kirmizi";
+		araba1.kapilar = 4;
+		araba1.tekerlekler = 8;
+		araba1.motor = "16";
+		araba1.model = "Renault";
+		System.out.println(araba1.tekerlekler);
+	}
+
+}
